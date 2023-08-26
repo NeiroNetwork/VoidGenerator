35f8fc1c389552bbafe7fe634a02070688059287
Nerahikada <hkshirorom@yahoo.co.jp>
2023-08-26 14:00:40 +0900

    バイオームを指定できたぞ！！

--- src/VoidGenerator.php
+++ src/VoidGenerator.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\VoidGenerator;
 
+use pocketmine\block\Block;
 use pocketmine\data\bedrock\BiomeIds;
 use pocketmine\world\ChunkManager;
-use pocketmine\world\format\BiomeArray;
 use pocketmine\world\format\Chunk;
+use pocketmine\world\format\SubChunk;
+use pocketmine\world\format\PalettedBlockArray;
 use pocketmine\world\generator\Generator;
 
 class VoidGenerator extends Generator{
@@ -17,8 +19,11 @@ class VoidGenerator extends Generator{
 	public function __construct(int $seed, string $preset){
 		parent::__construct($seed, $preset);
 
-		// fixme: バイオームが従来の PLAINS ではなく OCEAN になってる
 		$this->chunk = new Chunk([], false);
+		foreach($this->chunk->getSubChunks() as $y => $subChunk){
+			/** @link https://github.com/nhanaz-pm-pl/OnlyOneBiome/blob/master/src/Main.php */
+			$this->chunk->setSubChunk($y, new SubChunk(Block::EMPTY_STATE_ID, [], new PalettedBlockArray(BiomeIds::PLAINS)));
+		}
 	}
 
 	public function generateChunk(ChunkManager $world, int $chunkX, int $chunkZ) : void{
